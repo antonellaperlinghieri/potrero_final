0579508b3715a5b5122bf38071e449789996e277
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-13 20:19:09 -0300

    mejoras

--- inicio.php
+++ inicio.php
@@ -21,10 +21,10 @@
 <header>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="inicio.php"> <img src="assets/imagenes/logo.png" class = "logo" alt="" srcset="" style = "width : 50%";> </a>
+    <a class="navbar-brand" href="inicio.php"> <img src="assets/imagenes/logo.png" class = "logo" alt="" srcset="" style = "width : 50%; justify-content: center;"> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon">
-        <img src="https://cdn-icons-png.flaticon.com/512/5259/5259005.png" alt = "" width = "20px" style = "justify-content: center;">
+        <img src="https://cdn-icons-png.flaticon.com/512/5259/5259005.png" alt = "" width = "20px" >
       </span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
