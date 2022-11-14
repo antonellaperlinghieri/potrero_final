286915de49d5779550ee428ef1477bfba63941d6
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-14 15:06:20 -0300

    mejoras

--- inicio.php
+++ inicio.php
@@ -91,7 +91,7 @@
 </section>
 <div class="container">
     <div class="row">
-        <div class="col-md-3 col-lg-4 col-xl-6 mx-auto mb-4" style=" margin-left: auto;">
+        <div class="col" style=" margin-left: auto;">
         <h1 class="title col-lg-6 col-xl-6 mx-auto mb-4">SOBRE NOSOTROS</h1>
             Desde 1960, se ha convertido en un referente indiscutido a la hora de buscar diseño, calidad y confort. Durante más de 60 años nos hemos dedicado exclusivamente a fabricar y distribuir productos con la última tecnología, que
             brinden el máximo confort en movimiento. Contamos con una tienda de más de 3300 m², un espacio con la más amplia variedad de productos creados para el confort. La elección personalizada de la tapicería hace de cada producto una
