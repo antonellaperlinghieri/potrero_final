a5ac9e44f9e3267b87926deefbcb36de71a7da7b
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-14 09:12:33 -0300

    arreglos de conexion

--- producto.php
+++ producto.php
@@ -60,7 +60,7 @@
                         <div class="card" style="border: none;">
                             <?php
                               // 1) Conexion
-                              $conexion = mysqli_connect("h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "fdxnogh9ulra5ujq", "uycgjx70mu3k9vex", "qad7ort2fqaauzk2");
+                              include "assets/php/conexion.php";
                               $query='SELECT * FROM `productos` WHERE `id` = '.$_GET["id"];
                               // 3) Ejecutar la orden y obtenemos los registros
                               $datos= mysqli_query($conexion, $query);
@@ -129,7 +129,7 @@
       </div>
       <div class="modal-body" style = "margin: 12px;">
       <?php
-    $conexion = mysqli_connect("h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "fdxnogh9ulra5ujq", "uycgjx70mu3k9vex", "qad7ort2fqaauzk2");
+    include "assets/php/conexion.php";
     mysqli_select_db($conexion,"qad7ort2fqaauzk2");
         // 2) Almacenamos los datos del envío GET
         // a) generar variables para el id a utilizar
