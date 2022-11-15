901f51eb1873f440e1dc37f4269ee77af9fb7893
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-15 01:02:43 -0300

    se agrego el borrar y editar

--- borra_productos.php
+++ borra_productos.php
@@ -1,1 +1,8 @@
- 
+<?php
+    include "php/conexion.php";
+    $id = $_POST['id'];
+    $consulta = "DELETE FROM `productos` WHERE `id` =  $id";
+    $respuesta=mysqli_query($conexion, $consulta); 
+    echo "borramos el id ". $id;
+
+?>
--- lista.php
+++ lista.php
@@ -9,6 +9,9 @@
      <link rel="stylesheet" href="assets/css/styles.css" />
     <!-- BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <div class="container">
@@ -23,6 +26,7 @@
         <th>IMAGEN</th>
         <th>IMAGEN1</th>
         <th>IMAGEN2</th>
+        <th>EDITAR</th>
     </tr>
    </div>
     <?php
@@ -48,9 +52,47 @@
         <td><?php echo $reg['imagen']; ?></td>
         <td><?php echo $reg['imagen1']; ?></td>
         <td><?php echo $reg['imagen2']; ?></td>
+        <td>
+            <div class="btn-group">
+                <button class="btn btn-primary" onclick="editarProducto(<?php echo $reg['id']; ?>)">editar</button>
+                <button class="btn btn-danger" onclick="borrarProducto(<?php echo $reg['id']; ?>)">borrar</button>
+            </div>
+        </td>
+
         </tr>
     <?php } ?>
     </table>
     <button type="button" class="btn btn-outline-primary" id = "boton"><a href="inicio.php" id="button" style = "text-decoration : none;">VOLVER AL INICIO</a></button>
+    <script type="text/javascript">
+        function borrarProducto(id) {
+            console.log(id);
+            jQuery.ajax({
+                type: "POST",
+                url: "borra_productos.php",
+                data: "id="+id,
+                success: function(response) {			
+                    alert(response);
+                }
+            });
+        }
+
+        function editarProducto(id) {
+            window.location.href = "producto.php?id=" +id;
+
+        }
+    </script>
+
+ 
+
+
+
+
+
+
+
+<script src="assets/javascript/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
 </body>
 </html>
