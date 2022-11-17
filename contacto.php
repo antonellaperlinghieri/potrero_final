15bde25ae51520dff7a3a31ea588b12dd8ae8718
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-17 11:42:36 -0300

    probando el formulario

--- contacto.php
+++ contacto.php
@@ -12,4 +12,12 @@ if (mail ($to, $subject, $body, $from)) {
 } else { 
     echo '<p style="color: #c0392b;">Something went wrong, go back and try again!    </p>'; 
 }
+
+// Using default PHP curl library
+$ch = curl_init('https://realemail.expeditedaddons.com/?api_key=VC9QI3ZRP541T2606S8A787YFX43EG5KHWM9L0BND21JUO&email=email@example.org&fix_typos=false');
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 ?>
