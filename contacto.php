40594b8183e68903912fd69b8a623c348630492b
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-17 14:33:33 -0300

    arreglando el formulario

--- contacto.php
+++ contacto.php
@@ -5,16 +5,6 @@ $message = $_POST['mensaje'];
 $subject = "Mensaje desde la muebleria";
 $to = 'antonellaperlinghieri19988@gmail.com'; 
 
-$body = "De: $name\n E-Mail: $from\n Mensaje:\n $message";
-
-if (mail ($to, $subject, $body, $from)) { 
-    echo '<p style="color: #27ae60;">Your message has been sent!</p>';
-} else { 
-    echo '<p style="color: #c0392b;">Something went wrong, go back and try again!    </p>'; 
-}
-
-
-
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
