d22205500a3d26198e7497542a9feceb696aaa7f
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-17 11:50:48 -0300

    solucionando errores del formulario

--- contacto.php
+++ contacto.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $from = $_POST['email'];
 $message = $_POST['mensaje']; 
 $subject = "Mensaje desde la muebleria";
-$to = 'antonellaperlingheiri19988@gmail.com'; 
+$to = 'antonellaperlinghieri19988@gmail.com'; 
 
 $body = "De: $name\n E-Mail: $from\n Mensaje:\n $message";
 
@@ -13,11 +13,4 @@ if (mail ($to, $subject, $body, $from)) {
     echo '<p style="color: #c0392b;">Something went wrong, go back and try again!    </p>'; 
 }
 
-// Using default PHP curl library
-$ch = curl_init('https://realemail.expeditedaddons.com/?api_key=VC9QI3ZRP541T2606S8A787YFX43EG5KHWM9L0BND21JUO&email=email@example.org&fix_typos=false');
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
 ?>
