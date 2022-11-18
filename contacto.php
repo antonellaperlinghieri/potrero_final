a72ef56b3be97a4f06d982deb1c80a2b764cfb69
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 09:08:25 -0300

    implementando mailgun

--- contacto.php
+++ contacto.php
@@ -1,20 +1,15 @@
 <?php
+# Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-session_start();
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message =  $_POST['mensaje'];
-    # Instantiate the client.
-    $mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
-    $domain = "https://muebleriaanto.herokuapp.com/";
-    # Make the call to the client.
-    $result = $mgClient->sendMessage($domain, array(
-        'from'    => $name . ' <' . $email . '>',
-        'to'      => 'antonellaperlinghieri19988@gmail.com',
-        'subject' => 'MENSAJE ENVIADO CON EXITO',
-        'text'    => $message
-    ));
-}
+# Instantiate the client.
+$mgClient = new Mailgun('YOUR_API_KEY');
+$domain = "YOUR_DOMAIN_NAME";
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain, array(
+	'from'	=> 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
+	'to'	=> 'Baz <YOU@YOUR_DOMAIN_NAME>',
+	'subject' => 'Hello',
+	'text'	=> 'Testing some Mailgun awesomness!'
+));
 ?>
