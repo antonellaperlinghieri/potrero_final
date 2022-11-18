f20174b5e9fee95153fae797d95227b1b7dc6c59
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 09:28:00 -0300

    mailgun

--- contacto.php
+++ contacto.php
@@ -1,20 +1,17 @@
 <?php
-require '/vendor/autoload.php';
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-session_start();
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message =  $_POST['mensaje'];
-    # Instantiate the client.
-    $mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
-    $domain = "sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
-    # Make the call to the client.
-    $result = $mgClient->sendMessage($domain, array(
-        'from'    => $name . ' <' . $email . '>',
-        'to'      => 'antonellaperlinghieri19988@gmail.com',
-        'subject' => 'MENSAJE ENVIADO CON EXITO',
-        'text'    => $message
-    ));
-}
+
+# Instantiate the client.
+$mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
+$domain = "sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+	array('from'    => 'Mailgun Sandbox <postmaster@sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org>',
+		  'to'      => 'Heroku <app284077190@heroku.com>',
+		  'subject' => 'Hello Heroku',
+		  'template'    => 'antonella',
+		  'h:X-Mailgun-Variables'    => '{"test": "test"}'));
 ?>
