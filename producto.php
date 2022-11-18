f21327023c9db656c7d9cc7343fb2ba6c1d60083
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 18:09:09 -0300

    bottstrap

--- producto.php
+++ producto.php
@@ -9,6 +9,7 @@
         <!-- ESTILOS CSS -->
         <link rel="stylesheet" href="assets/css/styles.css" />
         <!-- BOOTSTRAP-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.1/font/bootstrap-icons.css">
         <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'>
