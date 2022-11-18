f149c775d0e845cb357e5f9f595502a337c06d55
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 09:16:23 -0300

    mailgun

--- contacto.php
+++ contacto.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit'])){
     $message =  $_POST['mensaje'];
     # Instantiate the client.
     $mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
-    $domain = "https://api.mailgun.net/v3/sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
+    $domain = "sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
     # Make the call to the client.
     $result = $mgClient->sendMessage($domain, array(
         'from'    => $name . ' <' . $email . '>',
