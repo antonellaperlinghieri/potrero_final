50f1d3e79624956723e7c10d0f31b0e9879700ff
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 11:12:01 -0300

    intentando impelemntar el formulario

--- contacto.php
+++ contacto.php
@@ -1,9 +1,19 @@
 <?php
-// Using default PHP curl library
-$ch = curl_init('https://realemail.expeditedaddons.com/?api_key=76IVPDCN329TQBMO9JF388AZ1420Y4ELR0UGH6SW15K5X7&email=antonellaperlinghieri19988@gmail.com.org&fix_typos=false');
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
+require '../vendor/autoload.php';
+use Mailgun\Mailgun;
+    //Obtenemos valores input formulario
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+    $para = 'antonellaperlinghieri19988@gmail.com';
+    $mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
+    $domain = "sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
+    
+    # Make the call to the client.
+    $result = $mgClient->sendMessage("$domain",
+         array('from'    => $from,
+                'to'      => $para,
+                'subject' => 'ENVIADO CON EXITO',
+                'template'    => 'antonella',
+                'h:X-Mailgun-Variables'    => '{"test": "test"}'));
 ?>
