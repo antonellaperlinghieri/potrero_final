74559a5150033749b692ce0b563440b09d50380b
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-18 09:13:14 -0300

    intentando hacer funcional mailgun

--- contacto.php
+++ contacto.php
@@ -1,15 +1,20 @@
 <?php
-# Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
+require '/vendor/autoload.php';
 use Mailgun\Mailgun;
-# Instantiate the client.
-$mgClient = new Mailgun('YOUR_API_KEY');
-$domain = "YOUR_DOMAIN_NAME";
-# Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-	'from'	=> 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-	'to'	=> 'Baz <YOU@YOUR_DOMAIN_NAME>',
-	'subject' => 'Hello',
-	'text'	=> 'Testing some Mailgun awesomness!'
-));
+session_start();
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message =  $_POST['mensaje'];
+    # Instantiate the client.
+    $mgClient = new Mailgun('fb12fa0ecd924e366a7a1c1b39261cf5-2de3d545-16924a94');
+    $domain = "https://api.mailgun.net/v3/sandbox135942d4cc284493910e6dfaab12dfce.mailgun.org";
+    # Make the call to the client.
+    $result = $mgClient->sendMessage($domain, array(
+        'from'    => $name . ' <' . $email . '>',
+        'to'      => 'antonellaperlinghieri19988@gmail.com',
+        'subject' => 'MENSAJE ENVIADO CON EXITO',
+        'text'    => $message
+    ));
+}
 ?>
