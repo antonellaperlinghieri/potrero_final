453fae1ed686cc2c4aa5e916d0bf9830b8f44573
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-23 20:46:49 -0300

    mejoras en el diseño

--- producto.php
+++ producto.php
@@ -22,30 +22,10 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="inicio.php"> <img src="assets/imagenes/logo.png" alt="" srcset="" style="width: 50%;" ; /> </a>
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon">
-                    <img src="https://cdn-icons-png.flaticon.com/512/5259/5259005.png" alt="" width="20px" />
-                </span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="inicio.php" id="link">INICIO</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="link" href="#productos">PRODUCTOS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="link" href="#sobre_nosotros">SOBRE NOSOTROS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="link" href="#galeria">GALERIA</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="link" href="tienda.php">TIENDA</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="link" href="#contactanos">CONTACTANOS</a>
                     </li>
                 </ul>
             </div>
