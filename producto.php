71c3a70f5171c40cf4af110c5966d2c35cc80266
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-23 20:49:44 -0300

    mejoras en el diseño

--- producto.php
+++ producto.php
@@ -22,6 +22,11 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="inicio.php"> <img src="assets/imagenes/logo.png" alt="" srcset="" style="width: 50%;" ; /> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                    <img src="https://cdn-icons-png.flaticon.com/512/5259/5259005.png" alt="" width="20px" />
+                </span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
