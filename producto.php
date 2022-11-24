f804da8a8bffa30693496b4eeb512eb510a64475
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-24 10:45:58 -0300

    se elimino mailgun formulario

--- producto.php
+++ producto.php
@@ -50,7 +50,7 @@
                         </div>
                     </div>
                     <!-- CAROUSEL -->
-                    <div class="col-lg-6 col-xl-6 mx-auto mb-4" style = "margin: 4%;">
+                    <div class="col-lg-6 col-xl-6 mx-auto mb-4" style = "margin: 2%;">
                         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-indicators">
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
