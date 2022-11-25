cab7c61b75962c0e04481ee817c9005a45d23fe7
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 16:17:55 -0300

    mejoras formulario

--- inicio.php
+++ inicio.php
@@ -264,29 +264,18 @@
                 </div>
                 <!--Grid row-->
                 <!--Grid row-->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control" />
-                            <label for="subject" class="">Subject</label>
-                        </div>
-                    </div>
-                </div>
-                <!--Grid row-->
-                <!--Grid row-->
-                <div class="row">
-                    <!--Grid column-->
-                    <div class="col-md-12">
+                <div class="col-md-12">
                         <div class="md-form">
                             <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
                             <label for="message">Tu Mensaje</label>
                         </div>
                     </div>
                 </div>
+                <div class="row">
                 <!--Grid row-->
             </form>
             <div class="text-center text-md-left">
-                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                <a class="btn btn-primary">ENVIAR CONSULTA</a>
             </div>
             <div class="status"></div>
         </div>
