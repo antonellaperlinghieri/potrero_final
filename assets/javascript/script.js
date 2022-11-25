cf855ab9286427ac4ce24b1f64aff302dba5da59
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 16:44:16 -0300

    mejoras

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -78,16 +78,3 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     modal.find('.modal-body input').val(recipient)
 })
 
-// ENVIO DE MAIL FORMULARIO DE CONSULTAS
-function sendEmail() {
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "antonellaperlinghieri25@gmail.com",
-        Password : "password",
-        To : 'antonellaperlinghieri19988@gmail.com',
-        From : document.getElementById('email').value,
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
--- inicio.php
+++ inicio.php
@@ -332,6 +332,20 @@
 <script src="assets/javascript/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-<script src="https://smtpjs.com/v3/smtp.js"></script>
+<script src="https://smtpjs.com/v3/smtp.js">
+    // ENVIO DE MAIL FORMULARIO DE CONSULTAS
+    function sendEmail() {
+        Email.send({
+            Host : "smtp.gmail.com",
+            Username : "antonellaperlinghieri25@outlook.com",
+            Password : "password",
+            To : 'antonellaperlinghieri19988@gmail.com',
+            From : document.getElementById('email').value,
+            Body : "And this is the body"
+        }).then(
+        message => alert(message)
+        );
+    }
+</script>
 </body>
 </html>
