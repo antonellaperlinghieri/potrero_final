e27e6664546977db20ee1a22a2b78eb25488cc96
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 16:14:32 -0300

    formulario

--- inicio.php
+++ inicio.php
@@ -233,65 +233,89 @@
   </div>
 </div>
 <!-- FORMULARIO DE CONSULTAS -->
+<!--Section: Contact v.2-->
 <section class="container mb-4">
+    <!--Section heading-->
     <h2 class="h1-responsive font-weight-bold text-center my-4">CONTACTANOS</h2>
-    <p class="text-center w-responsive mx-auto mb-5">ENVIANOS TU CONSULTA Y TE RESPONDEREMOS A LA BREVEDAD.</p>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">ENVIANOS TU CONSULTA Y TE RESPONDEREMOS A LA BREVEDAD</p>
     <div class="row">
+        <!--Grid column-->
         <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="" method="POST">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                <!--Grid row-->
                 <div class="row">
+                    <!--Grid column-->
                     <div class="col-md-6">
                         <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control">
+                            <input type="text" id="name" name="name" class="form-control" />
                             <label for="name" class="">Tu Nombre</label>
                         </div>
                     </div>
+                    <!--Grid column-->
+                    <!--Grid column-->
                     <div class="col-md-6">
                         <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control">
+                            <input type="text" id="email" name="email" class="form-control" />
                             <label for="email" class="">Tu Email</label>
                         </div>
                     </div>
+                    <!--Grid column-->
                 </div>
+                <!--Grid row-->
+                <!--Grid row-->
                 <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control">
+                            <input type="text" id="subject" name="subject" class="form-control" />
                             <label for="subject" class="">Subject</label>
                         </div>
                     </div>
                 </div>
+                <!--Grid row-->
+                <!--Grid row-->
                 <div class="row">
+                    <!--Grid column-->
                     <div class="col-md-12">
                         <div class="md-form">
                             <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message">Tu Consulta</label>
+                            <label for="message">Tu Mensaje</label>
                         </div>
                     </div>
                 </div>
+                <!--Grid row-->
             </form>
             <div class="text-center text-md-left">
-                <a class="btn btn-primary">Send</a>
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
             <div class="status"></div>
         </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
         <div class="col-md-3 text-center">
             <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                <li>
+                    <i class="fas fa-map-marker-alt fa-2x"></i>
                     <p>San Francisco, CA 94126, USA</p>
                 </li>
 
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                <li>
+                    <i class="fas fa-phone mt-4 fa-2x"></i>
                     <p>+ 01 234 567 89</p>
                 </li>
 
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <li>
+                    <i class="fas fa-envelope mt-4 fa-2x"></i>
                     <p>contact@mdbootstrap.com</p>
                 </li>
             </ul>
         </div>
+        <!--Grid column-->
     </div>
 </section>
+<!--Section: Contact v.2-->
+
 <!-- FOOTER -->
 <footer class="text-center text-lg-start bg-light text-muted" id = "contactanos">
     <section class="d-flex justify-content-center justify-content-lg-center p-4 border-bottom">
