01a11994b36006926f8e71fe2397240b57e8079b
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 10:35:00 -0300

    buscador

--- inicio.php
+++ inicio.php
@@ -107,7 +107,7 @@
   <div class="container">
   <h1 class = "title-productos"  id = "productos" style = "text-align : center;"> NUESTROS PRODUCTOS</h1>
   <div class="input-group">
-  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  <input type="search" class="form-control rounded" id = "authorsearch" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
   <button type="button" class="btn btn-outline-primary">BUSCAR PRODUCTO</button>
 </div>
 </div>
@@ -121,7 +121,7 @@
         $datos= mysqli_query($conexion, $query);
         // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
         while ($reg = mysqli_fetch_array($datos)) {?>
-          <div class="card col-md-3 col-lg-4 col-xl-4 mx-auto mb-4 " style = "border:none;">
+          <div class="author-card col-md-3 col-lg-4 col-xl-4 mx-auto mb-4 " style = "border:none;">
             <img class="card-img-top img-responsive" src="assets/imagenes/<?php echo $reg['imagen'] ?>" alt="" ><h3 class="card-title"><?php echo ucwords($reg['nombreproducto']) ?></h3>
               <span>$ <?php echo $reg['precio']; ?></span>
               <a href = "producto.php?id=<?php echo $reg['id']; ?>" class = "link">Ver producto</a>
