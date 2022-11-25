9f44ff53ec94d1881f26ac73257f0288d8b60daa
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 16:50:27 -0300

    mejoras

---
+++ mail.php
@@ -0,0 +1,34 @@
+<?php
+  if (isset($_POST['name']))
+    $name = $_POST['name'];
+  if (isset($_POST['email']))
+    $email = $_POST['email'];
+  if (isset($_POST['message']))
+    $message = $_POST['message'];
+  if ($name === '') {
+    echo "Name cannot be empty.";
+    die();
+  }
+  if ($email === '') {
+    echo "Email cannot be empty.";
+    die();
+  } else {
+    if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+      echo "Email format invalid.";
+      die();
+    }
+  }
+  if ($subject === '') {
+    echo "Subject cannot be empty.";
+    die();
+  }
+  if ($message === '') {
+    echo "Message cannot be empty.";
+    die();
+  }
+  $content = "From: $name \nEmail: $email \nMessage: $message";
+  $recipient = "antonellaperlinghieri19988@gmail.com";
+  $mailheader = "From: $email \r\n";
+  mail($recipient, $content, $mailheader) or die("Error!");
+  echo "Email sent!";
+?>
