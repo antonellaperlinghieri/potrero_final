2be30b9a37d3f3152cbda825616beeb66cff8bef
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 16:27:50 -0300

    mejoras

--- inicio.php
+++ inicio.php
@@ -275,15 +275,7 @@
                 </div>
                 <!--Grid row-->
                 <!--Grid row-->
-                <div class="row">
-                    <!--Grid column-->
-                    <div class="col-md-12">
-                        <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message"></label>
-                        </div>
-                    </div>
-                </div>
+               
                 <!--Grid row-->
             </form>
             <div class="text-center text-md-left">
