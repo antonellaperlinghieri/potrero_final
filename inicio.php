a1d5e3000ce70fbe532a8bca637d2d9d14806618
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-25 15:59:44 -0300

    hojadeestilosMDbootstrap

--- inicio.php
+++ inicio.php
@@ -20,11 +20,9 @@
     <link href="https://trial.chatcompose.com/static/trial/all/global/export/css/main.5b1bd1fd.css" rel="stylesheet">    
     <script async type="text/javascript" src="https://trial.chatcompose.com/static/trial/all/global/export/js/main.a7059cb5.js?user=trial_antonellaP&lang=ES" user="trial_antonellaP" lang="ES"></script> 
     <!-- MdBOOTSTRAP --> 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <link rel="stylesheet" href="node_modules/mdbootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="node_modules/mdbootstrap/css/mdb.min.css">
-    <link rel="stylesheet" href="node_modules/mdbootstrap/css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css" rel="stylesheet"/>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
 </head>
 <body>
     <!-- MENU Y BANNER -->
@@ -320,10 +318,8 @@
     </div>
 </footer>
 <!-- Footer -->
-<script type="text/javascript" src="node_modules/mdbootstrap/js/jquery.min.js"></script>
-<script type="text/javascript" src="node_modules/mdbootstrap/js/popper.min.js"></script>
-<script type="text/javascript" src="node_modules/mdbootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="node_modules/mdbootstrap/js/mdb.min.js"></script>
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"></script>
 <script src="assets/javascript/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
