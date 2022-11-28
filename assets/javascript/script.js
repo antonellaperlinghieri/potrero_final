d65d0b4edc4b73b10e6743a2758686c7e11b6bd9
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-28 11:27:41 -0300

    mejoras

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -77,3 +77,4 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
 })
+
