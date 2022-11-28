6c1f8ddbd28570efc23a32f29fd404fe13f0eb8c
Anto Perlinghieri <antonellaperlinghieri19988@gmail.com>
2022-11-28 10:53:40 -0300

    arreglos

--- producto.php
+++ producto.php
@@ -200,7 +200,7 @@
   $preference -> items = $productos;
   $preference -> save();
 ?>
-<div class="contenedor-btn"></div>
+<div class="contenedor-btn btn-primary"></div>
 <script src="https://sdk.mercadopago.com/js/v2"></script>
 <script>
   var public_key = 'TEST-5842c625-7611-41ac-95fe-13c0a4ef07f3';
@@ -215,9 +215,6 @@
     render: {
       container: '.contenedor-btn',
       label: 'COMPRAR',
-    },
-    theme: {
-      elementsColor: "rgb(5, 9, 65)";
     }
   });
 </script> <!--FIN MERCADO PAGO -->
